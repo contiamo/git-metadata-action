580fac7b44e0690f5dddcad06b531dbce878ba49
Lucas Roesler <roesler.lucas@gmail.com>
2021-05-14 14:02:06 +0200

    fix: remove plus sign from semver output
    When generating the semver output, we need to remove the plus sign so
    that it can be used as a docker tag.
    
    Signed-off-by: Lucas Roesler <roesler.lucas@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,17 @@ async function run(): Promise<void> {
   let sha = ''
   try {
     sha = await git(['rev-parse', '--short', 'HEAD'])
+
     core.setOutput('shortSHA', sha)
   } catch (error) {
     core.setFailed(error.message)
   }
 
   try {
+    // remove any `+` signs
+    const regex = /\+/i
     const semver = getSemanticVersion()
-    core.setOutput('semver', semver)
+    core.setOutput('semver', semver.replace(regex, '.'))
   } catch (error) {
     core.warning(error.message)
     // usually means there are no tags, return the shortsha instead
